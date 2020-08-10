1d09df2255db1b6e2f6744edf4e13207c8fb74fb
Utkarsh Baranwal <ukbaranwal@gmail.com>
2020-08-10 23:13:37 +0530

    Changes

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('nextdoor', 'postgres', 'postgres', {
+// const sequelize = new Sequelize('nextdoor', 'postgres', 'postgres', {
+//   dialect: 'postgres',
+//   host: 'localhost',
+// });
+
+const sequelize = new Sequelize(process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
   dialect: 'postgres',
-  host: 'localhost',
+  host: process.env.RDS_HOSTNAME,
+  port: process.env.RDS_PORT,
 });
 
 // const sequelize = new Sequelize('d2qu01kjbd9km1', 'tdjedudbblrkkp', '3eb5d35f5c4911247887979f7fdc08ca18f5176e209f8d061b777b339e085f86', {
