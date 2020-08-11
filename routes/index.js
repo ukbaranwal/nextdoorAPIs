d769f2b531aafb6b2fbb994468dedf46149e08bb
Utkarsh Baranwal <ukbaranwal@gmail.com>
2020-08-12 01:04:37 +0530

    Changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('<html>Welcome to Next Door</html>');
 });
 
 module.exports = router;
