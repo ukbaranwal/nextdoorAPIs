6c3c3b5a40ab35a35075a116ba9fb2598b049b70
Utkarsh Baranwal <ukbaranwal@gmail.com>
2020-09-24 23:54:02 +0530

    Bug Fix

--- util/database.js
+++ util/database.js
@@ -11,9 +11,9 @@ const Sequelize = require('sequelize');
 //   port: process.env.RDS_PORT,
 // });
 
-// const sequelize = new Sequelize('d2qu01kjbd9km1', 'tdjedudbblrkkp', '3eb5d35f5c4911247887979f7fdc08ca18f5176e209f8d061b777b339e085f86', {
-//   dialect: 'postgres',
-//   host: 'ec2-34-236-215-156.compute-1.amazonaws.com',
-// });
+const sequelize = new Sequelize('d2qu01kjbd9km1', 'tdjedudbblrkkp', '3eb5d35f5c4911247887979f7fdc08ca18f5176e209f8d061b777b339e085f86', {
+  dialect: 'postgres',
+  host: 'ec2-34-236-215-156.compute-1.amazonaws.com',
+});
 
 module.exports = sequelize;
